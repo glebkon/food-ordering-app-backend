47a48ceef694f5ca8262e991dcf01f470d668f8e
glebkon <kondakovgleb12@gmail.com>
2024-03-21 17:59:12 +0200

    adding user profile feature

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -25,3 +25,30 @@ export const validateMyUserRequest = [
   body("city").isString().notEmpty().withMessage("City must be a string"),
   handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName")
+    .isString()
+    .notEmpty()
+    .withMessage("Restaurant name must be a string"),
+  body("country").isString().notEmpty().withMessage("Country must be a string"),
+  body("city").isString().notEmpty().withMessage("City must be a string"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery price must be a positive number"),
+  body("deliveryPrice")
+    .isInt({ min: 0 })
+    .withMessage("Delivery price must be a positive integer"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu item price must be a positive number"),
+  handleValidationErrors,
+];
